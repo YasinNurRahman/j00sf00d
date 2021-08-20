e9fd46198634a73fd0683fc0f992bcb5274bec81
YasinNurRahman <yasinnurrahman@gmail.com>
2021-08-20 19:42:59 +0600

    reup

---
+++ resources/js/script.js
@@ -0,0 +1,53 @@
+
+/* for the Sticky navigation */
+$('.js--section-features').waypoint(function(direction) {
+    if (direction == "down") {
+        $('nav').addClass('sticky');
+    } else {
+        $('nav').removeClass('sticky');
+    }
+}, {
+    offset: '60px'
+});
+
+/* Scroll on buttons */
+$('.js--scroll-to-plan').click(function(){
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+});
+$('.js--scroll-to-first-part').click(function(){
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+});
+
+$(function() {
+    // This will select everything with the class smoothScroll
+    // This should prevent problems with carousel, scrollspy, etc...
+    $('.smoothScroll').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000); // The number here represents the speed of the scroll in milliseconds
+          return false;
+        }
+      }
+    });
+  });
+  
+  // Change the speed to whatever you want
+  // Personally i think 1000 is too much
+  // Try 800 or below, it seems not too much but it will make a difference
+/* 
+var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})
+
+
+
+$(document).ready(function() {
+
+});
+*/
